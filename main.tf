9015a66e3d669a5843fde3ed2c9e6b27ee156432
nrayavaram <91076970+nrayavaram@users.noreply.github.com>
2021-10-08 12:40:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ resource "google_compute_network" "vpc_network" {
 #  project                 = "my-project-name"
   name                    = "vpc-network"
   auto_create_subnetworks = true
-  mtu                     = 1460
+  mtu                     = 1500
 }
